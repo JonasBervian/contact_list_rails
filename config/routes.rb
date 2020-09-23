467690b268ea6461ca4a6e604a87f80798ac2a1b
JonasBervian <jonasgabrielbervian@gmail.com>
2020-09-23 14:26:08 -0300

    Criado o cadastro do usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :contacts
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
 
+  resources :contacts
+  resources :users, only: [:new, :create]
 end
