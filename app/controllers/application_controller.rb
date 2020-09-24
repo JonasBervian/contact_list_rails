7c3d4848f9f1ee4bd3f391c89c4ba35b5b64ee7e
JonasBervian <jonasgabrielbervian@gmail.com>
2020-09-24 16:47:16 -0300

    Conclusão do app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
 
     include SessionsHelper
 
+    private
+        def require_logged_in_user
+            unless user_signed_in?
+                flash[:danger] = 'Área restrita. Realize o login!'
+                redirect_to entrar_path
+            end
+        end
 end
--- db/migrate/20200922182643_create_contacts.rb
+++ db/migrate/20200922182643_create_contacts.rb
@@ -2,7 +2,7 @@ class CreateContacts < ActiveRecord::Migration[5.2]
   def change
     create_table :contacts do |t|
       t.string :name
-      t.string :phone
+      t.string :phone 
 
       t.timestamps
     end
